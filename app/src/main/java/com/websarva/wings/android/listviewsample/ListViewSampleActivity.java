2af836a42f0e4bfc0018c13c89543e99f2b11dee
disk3457 <dsakkyan0304@yahoo.co.jp>
2018-05-30 10:09:09 +0900

    5月30日

--- app/src/main/java/com/websarva/wings/android/listviewsample/ListViewSampleActivity.java
+++ app/src/main/java/com/websarva/wings/android/listviewsample/ListViewSampleActivity.java
@@ -2,6 +2,11 @@ package com.websarva.wings.android.listviewsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class ListViewSampleActivity extends AppCompatActivity {
 
@@ -9,5 +14,22 @@ public class ListViewSampleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view_sample);
+
+        ListView lvMenu = findViewById(R.id.lvMenu);
+
+        lvMenu.setOnItemClickListener(new ListItemClickListener());
+
+    }
+
+    private class ListItemClickListener implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            String item = (String) parent.getItemAtPosition(position);
+
+            String show = "あなたが選んだ定食: " + item;
+
+            Toast.makeText(ListViewSampleActivity.this, "", Toast.LENGTH_LONG).show();
+        }
     }
 }
